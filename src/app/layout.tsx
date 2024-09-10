043880e61666ead953de98d77784815f5c07bc25
Mohamed Ashif K M <mohamedashif1911@gmail.com>
2024-09-10 22:52:38 +0530

    vercel deployment done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import '@/app/ui/globals.css';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Daily2Min',
+  description: 'The official Daily2Min app. A app to develop journaling Habit.',
+  icons: {
+    icon: '/logo.png', // or .png, .svg etc.
+  },
+};
 
 export default function RootLayout({
   children,
